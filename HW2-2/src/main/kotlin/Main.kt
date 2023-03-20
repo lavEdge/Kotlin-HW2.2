30b400aec87145a70347ebb2b340188b37ca52db
EDGE <temachki@gmail.com>
2023-03-21 02:46:28 +0300

    HW2.2 add exceprions

--- HW2-2/src/main/kotlin/Main.kt
+++ HW2-2/src/main/kotlin/Main.kt
@@ -1,7 +1,6 @@
 fun main() {
-    val likes = 631
-
-    val result = if (likes === 1 || likes % 10 === 1 && likes > 11) "человеку" else "людям"
+    val likes = 211
+    val result = if (likes === 1 || likes % 10 === 1 && (likes - 11) % 100 != 0 ) "человеку" else "людям"
 
     println("Понравилось $likes $result")
 }
